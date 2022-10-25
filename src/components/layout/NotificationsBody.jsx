12a22cffb460dfbe195faaa3a3a362c4e7583ae5
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2022-10-25 16:18:25 -0500

    Array for notification

--- src/components/layout/NotificationsBody.jsx
+++ src/components/layout/NotificationsBody.jsx
@@ -1,16 +1,8 @@
 import '../../scss/layout/NotificationsBody.scss';
 import { Notification } from '../Notification';
-import { Users } from '../Users';
+import { UsersNotification } from '../UsersNotification';
 
 function NotificationsBody() {
-    const typeDates = {
-        'm': 'm',
-        'd': ' day',
-        'ds': ' days',
-        'w': ' week',
-        'ws': ' weeks'
-    }
-
     const notificationType = {
         'd': 'Default',
         'f': 'Followed',
@@ -22,65 +14,65 @@ function NotificationsBody() {
     return (
         <div className='Notifications d-flx f-column'>
             <Notification
-                type={notificationType.d}
-                name={Users[0].name}
-                avatar={Users[0].avatar}
-                description='reacted to your recent post'
-                theme='My first tournament today!'
-                read={false}
-                elapsed={1}
+                type={UsersNotification[0].type}
+                name={UsersNotification[0].name}
+                avatar={UsersNotification[0].avatar}
+                description={UsersNotification[0].description}
+                theme={UsersNotification[0].theme}
+                read={UsersNotification[0].read}
+                elapsed={UsersNotification[0].elapsed}
             />
             <Notification
-                type={notificationType.f}
-                name={Users[1].name}
-                avatar={Users[1].avatar}
-                read={false}
-                elapsed={1}
+                type={UsersNotification[1].type}
+                name={UsersNotification[1].name}
+                avatar={UsersNotification[1].avatar}
+                read={UsersNotification[1].read}
+                elapsed={UsersNotification[1].elapsed}
             />
             <Notification
-                type={notificationType.g}
-                name={Users[2].name}
-                avatar={Users[2].avatar}
-                description='has joined tour group'
-                theme='Chess Club'
-                read={false}
-                elapsed={1}
+                type={UsersNotification[2].type}
+                name={UsersNotification[2].name}
+                avatar={UsersNotification[2].avatar}
+                description={UsersNotification[2].description}
+                theme={UsersNotification[2].theme}
+                read={UsersNotification[2].read}
+                elapsed={UsersNotification[2].elapsed}
                 typeDate='d'
             />
             <Notification
-                type={notificationType.m}
-                name={Users[3].name}
-                avatar={Users[3].avatar}
-                read={true}
-                elapsed={5}
+                type={UsersNotification[3].type}
+                name={UsersNotification[3].name}
+                avatar={UsersNotification[3].avatar}
+                read={UsersNotification[3].read}
+                elapsed={UsersNotification[3].elapsed}
                 typeDate='d'
             />
             <Notification
-                type={notificationType.p}
-                name={Users[4].name}
-                avatar={Users[4].avatar}
-                read={true}
-                elapsed={1}
+                type={UsersNotification[4].type}
+                name={UsersNotification[4].name}
+                avatar={UsersNotification[4].avatar}
+                read={UsersNotification[4].read}
+                elapsed={UsersNotification[4].elapsed}
                 typeDate='w'
             />
             <Notification
-                type={notificationType.d}
-                name={Users[5].name}
-                avatar={Users[5].avatar}
-                description='reacted to your recent post'
-                theme='5 end-game strategies to increase your win rate'
-                read={true}
-                elapsed={2}
+                type={UsersNotification[5].type}
+                name={UsersNotification[5].name}
+                avatar={UsersNotification[5].avatar}
+                description={UsersNotification[5].description}
+                theme={UsersNotification[5].theme}
+                read={UsersNotification[5].read}
+                elapsed={UsersNotification[5].elapsed}
                 typeDate='w'
             />
             <Notification
-                type={notificationType.g}
-                name={Users[6].name}
-                avatar={Users[6].avatar}
-                description='left the group'
-                theme='Chess Club'
-                read={true}
-                elapsed={2}
+                type={UsersNotification[6].type}
+                name={UsersNotification[6].name}
+                avatar={UsersNotification[6].avatar}
+                description={UsersNotification[6].description}
+                theme={UsersNotification[6].theme}
+                read={UsersNotification[6].read}
+                elapsed={UsersNotification[6].elapsed}
                 typeDate='w'
             />
         </div>
