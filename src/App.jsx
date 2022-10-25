a1c60c39c61ffa47b5d8959434aaae3e28651de8
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2022-10-25 17:30:37 -0500

    Scroll bar in the notification area

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { NotificationsBody } from './components/layout/NotificationsBody';
 function App() {
     return (
         <main id='primary-content' className='d-grd'>
-            <section className='NotificationsContainer'>
+            <section className='NotificationsContainer d-grd'>
                 <ContainerHeader />
                 <NotificationsBody />
             </section>
